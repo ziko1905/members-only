bcba352d8b448cd6d87574e70b698d5a70bb6e7f
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-12 18:38:44 +0200

    Create 'populate.js'

---
+++ models/populate.js
@@ -0,0 +1,43 @@
+const { Client } = require("pg");
+const fs = require("node:fs");
+const { connectionString } = require("pg/lib/defaults");
+
+const CREATE_MESSAGES_TABLE = `CREATE TABLE messages (
+                           id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                           timestamp DATE,
+                           title VARCHAR( 63 ),
+                           content VARCHAR( 1027 ),
+                           author_id INTEGER
+                           );`;
+
+const CREATE_USERS_TABLE = `CREATE TABLE users (
+                        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                        full_name VARCHAR ( 255 ),
+                        email VARCHAR ( 255 ),
+                        member BOOL,
+                        admin BOOL
+                        );`;
+
+function getSQL() {
+  try {
+    const CREATE_SESSION_TABLE = fs.readFileSync(
+      "./node_modules/connect-pg-simple/table.sql"
+    );
+    return [...arguments].join(" ") + CREATE_SESSION_TABLE;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function main() {
+  console.log("sending");
+  const client = new Client({
+    connectionString: process.argv[2],
+  });
+  await client.connect();
+  await client.query(getSQL(CREATE_MESSAGES_TABLE, CREATE_USERS_TABLE));
+  await client.end();
+  console.log("done");
+}
+
+main();
